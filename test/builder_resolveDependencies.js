bc0d33a03bfceb52276a32382e4ad9caccdc00d5
Rob Richardson <robrich@robrich.org>
2014-03-10 18:58:17 -0700

    a new approach to resolving dependencies that works very well

--- test/builder_resolveDependencies.js
+++ test/builder_resolveDependencies.js
@@ -21,6 +21,16 @@ describe('lib/builder/', function() {
 			};
 		};
 
+		var verifyDependencies = function (task, depArray) {
+			var message = task.name+' -> expected: \''+depArray.join('\', \'')+
+				'\' actual: \''+task.before.join('\', \'')+'\'';
+			task.before.length.should.equal(depArray.length, message);
+			var i = 0;
+			for (i = 0; i < task.before.length; i++) {
+				task.before[i].should.equal(depArray[i], message);
+			}
+		};
+
 		it('should run successfully', function(done) {
 
 			// arrange
@@ -50,8 +60,8 @@ describe('lib/builder/', function() {
 			resolveDependencies(builder, allTasks, function (err) {
 
 				// assert
-				allTasks.one.before.length.should.equal(0);
-				allTasks.two.before.length.should.equal(0);
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, []);
 				should.not.exist(err);
 
 				done();
@@ -71,9 +81,8 @@ describe('lib/builder/', function() {
 			resolveDependencies(builder, allTasks, function (err) {
 
 				// assert
-				allTasks.one.before.length.should.equal(0);
-				allTasks.two.before.length.should.equal(1);
-				allTasks.two.before[0].should.equal('one');
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
 				should.not.exist(err);
 
 				done();
@@ -121,7 +130,7 @@ describe('lib/builder/', function() {
 
 			// arrange
 			var nested = parallel('two', 'three');
-			var builder = parallel('one','four');
+			var builder = parallel('one', nested, 'four');
 			var allTasks = {
 				one: makeTask('one'),
 				two: makeTask('two'),
@@ -133,10 +142,10 @@ describe('lib/builder/', function() {
 			resolveDependencies(builder, allTasks, function (err) {
 
 				// assert
-				allTasks.one.before.length.should.equal(0);
-				allTasks.two.before.length.should.equal(0);
-				allTasks.three.before.length.should.equal(0);
-				allTasks.four.before.length.should.equal(0);
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, []);
+				verifyDependencies(allTasks.three, []);
+				verifyDependencies(allTasks.four, []);
 				should.not.exist(err);
 
 				done();
@@ -159,29 +168,242 @@ describe('lib/builder/', function() {
 			resolveDependencies(builder, allTasks, function (err) {
 
 				// assert
-				allTasks.one.before.length.should.equal(0);
-				allTasks.two.before.length.should.equal(1);
-				allTasks.two.before[0].should.equal('one');
-				allTasks.three.before.length.should.equal(1);
-				allTasks.three.before[0].should.equal('one');
-				allTasks.four.before.length.should.equal(2);
-				allTasks.four.before[0].should.equal('two');
-				allTasks.four.before[0].should.equal('three');
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['one']);
+				verifyDependencies(allTasks.four, ['two','three']);
 				should.not.exist(err);
 
 				done();
 			});
 		});
 
-		/*
-		x parallel nested in parallel
-		. parallel nested in series
-		- series nested in parallel
-		- series nested in series
-		- 3 levels (various configurations)
-		- parallel(series('one','two'),series('three','four'))
-		- series(parallel('one','two'),parallel('three','four'))
-		*/
+		it('should resolve series nested inside parallel dependencies', function(done) {
+
+			// arrange
+			var nested = series('two', 'three');
+			var builder = parallel('one', nested, 'four');
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, []);
+				verifyDependencies(allTasks.three, ['two']);
+				verifyDependencies(allTasks.four, []);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series nested inside series dependencies', function(done) {
+
+			// arrange
+			var nested = series('two', 'three');
+			var builder = series('one', nested, 'four');
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['two']);
+				verifyDependencies(allTasks.four, ['three']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series,series nested inside series dependencies', function(done) {
+
+			// arrange
+			var nested1 = series('one', 'two');
+			var nested2 = series('three', 'four');
+			var builder = series(nested1, nested2);
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['two']);
+				verifyDependencies(allTasks.four, ['three']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve parallel,parallel nested inside series dependencies', function(done) {
+
+			// arrange
+			var nested1 = parallel('one', 'two');
+			var nested2 = parallel('three', 'four');
+			var builder = series(nested1, nested2);
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, []);
+				verifyDependencies(allTasks.three, ['one', 'two']);
+				verifyDependencies(allTasks.four, ['one', 'two']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series, parallel nested inside series dependencies', function(done) {
+
+			// arrange
+			var nested1 = series('one', 'two');
+			var nested2 = parallel('three', 'four');
+			var builder = series(nested1, nested2);
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['two']);
+				verifyDependencies(allTasks.four, ['two']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series inside parallel inside series dependencies', function(done) {
+
+			// arrange
+			var grandNested = series('three', 'four');
+			var nested = parallel('two', grandNested, 'five');
+			var builder = series('one', nested, 'six');
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four'),
+				five: makeTask('five'),
+				six: makeTask('six')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['one']);
+				verifyDependencies(allTasks.four, ['three']);
+				verifyDependencies(allTasks.five, ['one']);
+				verifyDependencies(allTasks.six, ['two', 'four', 'five']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series inside series inside series dependencies', function(done) {
+
+			// arrange
+			var grandNested = series('three', 'four');
+			var nested = series('two', grandNested, 'five');
+			var builder = series('one', nested, 'six');
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four'),
+				five: makeTask('five'),
+				six: makeTask('six')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, ['one']);
+				verifyDependencies(allTasks.three, ['two']);
+				verifyDependencies(allTasks.four, ['three']);
+				verifyDependencies(allTasks.five, ['four']);
+				verifyDependencies(allTasks.six, ['five']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
+
+		it('should resolve series inside parallel inside series with builder preNested dependencies', function(done) {
+
+			// arrange
+			var preNested = parallel('one', 'two');
+			var grandNested = series('four', 'five');
+			var nested = parallel('three', grandNested, 'six');
+			var builder = series(preNested, nested);
+			var allTasks = {
+				one: makeTask('one'),
+				two: makeTask('two'),
+				three: makeTask('three'),
+				four: makeTask('four'),
+				five: makeTask('five'),
+				six: makeTask('six')
+			};
+
+			// act
+			resolveDependencies(builder, allTasks, function (err) {
+
+				// assert
+				verifyDependencies(allTasks.one, []);
+				verifyDependencies(allTasks.two, []);
+				verifyDependencies(allTasks.three, ['one', 'two']);
+				verifyDependencies(allTasks.four, ['one', 'two']);
+				verifyDependencies(allTasks.five, ['four']);
+				verifyDependencies(allTasks.six, ['one', 'two']);
+				should.not.exist(err);
+
+				done();
+			});
+		});
 
 	});
 });
