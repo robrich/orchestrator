17af78bfe940433f45fed21a7c6e4c2b8250dfaa
Blaine Bublitz <blaine@iceddev.com>
2014-03-21 18:43:57 -0700

    event emitting registry

--- registry/Default.js
+++ registry/Default.js
@@ -1,19 +1,37 @@
 'use strict';
 
+var EE = require('events').EventEmitter;
+var util = require('util');
+
 function DefaultRegistry(){
   if(!(this instanceof DefaultRegistry)){
     return new DefaultRegistry();
   }
 
+  EE.call(this);
+
   this.tasks = {};
 }
 
-DefaultRegistry.prototype.get = function(key){
-  return this.tasks[key];
+util.inherits(DefaultRegistry, EE);
+
+DefaultRegistry.prototype.get = function(name){
+  return this.tasks[name];
 };
 
-DefaultRegistry.prototype.set = function(key, value){
-  this.tasks[key] = value;
+DefaultRegistry.prototype.set = function(name, fn){
+  var evt = {
+    name: name,
+    fn: fn
+  };
+
+  if(this.tasks[name]){
+    this.emit('update', evt);
+  } else {
+    this.emit('new', evt);
+  }
+
+  this.tasks[name] = fn;
 };
 
 module.exports = DefaultRegistry;
--- test/index.js
+++ test/index.js
@@ -6,23 +6,30 @@ var expect = chai.expect;
 
 var Orchestrator = require('../');
 
+var prettyTime = require('pretty-hrtime');
+
 describe('description', function(){
 
   var o = new Orchestrator();
 
-  o.task('task1', function(){
+  o.task('task1', function(cb){
     console.log('task1');
 
-    return 1;
+    cb(null, 1);
   });
 
-  o.task('task2', function(){
+  o.task('task2', function(cb){
     console.log('task2');
 
-    return 2;
+    cb(null, 2);
   });
 
   it('description', function(){
+    function logTime(time){
+      console.log(prettyTime(time));
+    }
+    o.registry.on('start', logTime);
+    o.registry.on('end', logTime);
     o.series('task1', 'task2')(console.log);
     // expect(target);
   });
